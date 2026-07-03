88cbeec236a3c18f39669b6ce4fa2ebb5a656c57
Selvakumar <r.selvakumar@stackroute.in>
2026-07-03 15:24:12 +0530

    project added

---
+++ loyaltypoints/src/main/java/com/learn/loyalty/LoyaltypointsApplication.java
@@ -0,0 +1,13 @@
+package com.learn.loyalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltypointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltypointsApplication.class, args);
+	}
+
+}
